9a4f127573e5c3d1f1968c7fe93b9c1303ef4c7a
Hamza <hamooz7670@gmail.com>
2019-10-25 09:32:51 -0400

    Added Math Code

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,16 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    int pickedNumber;
+
+pickedNumber = (int) Math.random();
+
+//
+
+
+
+    }
+}
