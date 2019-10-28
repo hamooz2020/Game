a910406b4b4aa519c2980fffd1ae0ec8c5e45844
Hamza <hamooz7670@gmail.com>
2019-10-28 09:38:36 -0400

    Created a scanner and int

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,16 +1,46 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        int random, number;
+
+        random = (int) (Math.random()* 100);
+        System.out.println("I am thinking of a number between 1 and 30, can you guess it?");
+        System.out.println(random);
+
+do {
+    System.out.println("I am thinking of a number between 1 and 30, can you guess it?");
+
+}
+    while();
+
+
+
+
+
+
+
+
+
+
+
+
+
     int pickedNumber;
+        pickedNumber = (int) (Math.random()*2);
 
-pickedNumber = (int) Math.random();
-
+        for(int i = 0; i <20; i++) {
+            pickedNumber = (int) (Math.random()*30)+1;
+            System.out.println(pickedNumber);
 //
-
-
-
+        }
     }
 }
