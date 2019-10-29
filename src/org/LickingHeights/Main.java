36be42c99e6541bc5b422b487500d497dba94b36
Hamza <hamooz7670@gmail.com>
2019-10-29 09:42:24 -0400

    Made the game run

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -6,23 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int random;
+        int pickedNumber;
 
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
-        int random, number;
 
-        random = (int) (Math.random()* 100);
+
+        random = (int) (Math.random()* 30);
         System.out.println("I am thinking of a number between 1 and 30, can you guess it?");
-        System.out.println(random);
-
-do {
-    System.out.println("I am thinking of a number between 1 and 30, can you guess it?");
-
-}
-    while();
 
 
+        pickedNumber = keyboard.nextInt();
+        while (pickedNumber != random) {
+
+            if(pickedNumber < random){
+                System.out.println(pickedNumber+ " is too low");
+                 pickedNumber = keyboard.nextInt();
+            }
+             else if (pickedNumber > random) {
+                System.out.println(pickedNumber + " is too high!");
+                pickedNumber = keyboard.nextInt();
+            }
+
+                if(pickedNumber == random) {
+                    System.out.println("You are correct!");
+                }
+                
+
+        }
 
 
 
@@ -34,13 +47,13 @@ do {
 
 
 
-    int pickedNumber;
-        pickedNumber = (int) (Math.random()*2);
 
-        for(int i = 0; i <20; i++) {
-            pickedNumber = (int) (Math.random()*30)+1;
-            System.out.println(pickedNumber);
+//        pickedNumber = (int) (Math.random()*2);
+
+ //       for(int i = 0; i <20; i++) {
+ //           pickedNumber = (int) (Math.random()*30)+1;
+ //           System.out.println(pickedNumber);
 //
         }
     }
-}
+
